765c6db686edd7bfe2fc6427bed036acbd339820
Ramiz Rashidov <148320174+Ramiz22411@users.noreply.github.com>
2024-06-12 14:47:47 +0500

    Add files via upload
    DB  conf

---
+++ database/engin.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from database.models import Base
+
+from common.text_for_db import categories, description_for_info_pages
+from database.orm_query import orm_create_categories, orm_add_banner_description
+
+load_dotenv(find_dotenv())
+
+engin = create_async_engine(os.getenv('DB_POSTGRES'), echo=True)
+
+session_maker = async_sessionmaker(bind=engin, class_=AsyncSession, expire_on_commit=False)
+
+
+async def create_db():
+    async with engin.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)
+
+
+async def drop_db():
+    async with engin.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
