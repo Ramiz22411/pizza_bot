0656d44f66176e2e3fd5bce80db1740d5f7fc627
Ramiz Rashidov <148320174+Ramiz22411@users.noreply.github.com>
2024-06-12 14:46:50 +0500

    Add files via upload
    keybord button

---
+++ kbds/inline.py
@@ -0,0 +1,144 @@
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+class MenuCallBack(CallbackData, prefix='menu'):
+    level: int
+    menu_name: str
+    category: int | None = None
+    page: int = 1
+    product_id: int | None = None
+
+
+def get_user_main_btns(*, level: int, sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+    btns = {
+        "Products 🍕": "catalog",
+        "Cart 🛒": "cart",
+        "About ℹ️": "about",
+        "Payment 💰": "payment",
+        "Delivery ⛵": "shipping",
+    }
+    for text, menu_name in btns.items():
+        if menu_name == 'catalog':
+            keyboard.add(InlineKeyboardButton(
+                text=text,
+                callback_data=MenuCallBack(level=level + 1, menu_name=menu_name).pack()))
+        elif menu_name == 'cart':
+            keyboard.add(InlineKeyboardButton(text=text, callback_data=MenuCallBack(
+                level=3, menu_name=menu_name).pack()))
+        else:
+            keyboard.add(InlineKeyboardButton(text=text, callback_data=MenuCallBack(
+                level=level, menu_name=menu_name).pack()))
+
+    return keyboard.adjust(*sizes).as_markup()
+
+
+def get_user_catalog_btns(*, level: int, categories: list, sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.add(InlineKeyboardButton(
+        text='back',
+        callback_data=MenuCallBack(level=level - 1, menu_name='main').pack()))
+    keyboard.add(InlineKeyboardButton(text='Cart 🛒',
+                                      callback_data=MenuCallBack(level=3, menu_name='cart').pack()))
+
+    for category in categories:
+        keyboard.add(InlineKeyboardButton(
+            text=category.name,
+            callback_data=MenuCallBack(level=level + 1, menu_name=category.name, category=category.id).pack()))
+
+    return keyboard.adjust(*sizes).as_markup()
+
+
+def get_products_btns(
+        *,
+        level: int,
+        category: int,
+        page: int,
+        pagination_btns: dict,
+        product_id: int,
+        sizes: tuple[int] = (2, 1)
+):
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.add(InlineKeyboardButton(text='back', callback_data=MenuCallBack(
+        level=level - 1, menu_name='catalog').pack()))
+    keyboard.add(InlineKeyboardButton(text='Cart 🛒', callback_data=MenuCallBack(
+        level=3, menu_name='cart').pack()))
+    keyboard.add(InlineKeyboardButton(text='Buy 💵', callback_data=MenuCallBack(
+        level=level, menu_name='add_to_cart', product_id=product_id).pack()))
+
+    keyboard.adjust(*sizes)
+
+    row = []
+    for text, menu_name in pagination_btns.items():
+        if menu_name == 'next':
+            row.append(InlineKeyboardButton(text=text, callback_data=MenuCallBack(
+                level=level, menu_name=menu_name,
+                category=category, page=page + 1).pack()))
+        elif menu_name == 'previous':
+            row.append(InlineKeyboardButton(text=text, callback_data=MenuCallBack(
+                level=level, menu_name=menu_name,
+                category=category, page=page - 1).pack()))
+    return keyboard.row(*row).as_markup()
+
+
+def get_user_cart(
+        *,
+        level: int,
+        page: int | None,
+        pagination_btns: dict | None,
+        product_id: int | None,
+        sizes: tuple[int] = (3,)
+):
+    keyboard = InlineKeyboardBuilder()
+    if page:
+        keyboard.add(InlineKeyboardButton(text='Delete',
+                                          callback_data=MenuCallBack(level=level, menu_name='delete',
+                                                                     product_id=product_id, page=page).pack()))
+        keyboard.add(InlineKeyboardButton(text='-1',
+                                          callback_data=MenuCallBack(level=level, menu_name='decrement',
+                                                                     product_id=product_id, page=page).pack()))
+        keyboard.add(InlineKeyboardButton(text='+1',
+                                          callback_data=MenuCallBack(level=level, menu_name='increment',
+                                                                     product_id=product_id, page=page).pack()))
+
+        keyboard.adjust(*sizes)
+
+        row = []
+        for text, menu_name in pagination_btns.items():
+            if menu_name == "next":
+                row.append(InlineKeyboardButton(text=text,
+                                                callback_data=MenuCallBack(level=level, menu_name=menu_name,
+                                                                           page=page + 1).pack()))
+            elif menu_name == "previous":
+                row.append(InlineKeyboardButton(text=text,
+                                                callback_data=MenuCallBack(level=level, menu_name=menu_name,
+                                                                           page=page - 1).pack()))
+
+        keyboard.row(*row)
+
+        row2 = [
+            InlineKeyboardButton(text='Home 🏠',
+                                 callback_data=MenuCallBack(level=0, menu_name='main').pack()),
+            InlineKeyboardButton(text='Order',
+                                 callback_data=MenuCallBack(level=0, menu_name='order').pack()),
+        ]
+        return keyboard.row(*row2).as_markup()
+    else:
+        keyboard.add(
+            InlineKeyboardButton(text='Home 🏠',
+                                 callback_data=MenuCallBack(level=0, menu_name='main').pack()))
+
+        return keyboard.adjust(*sizes).as_markup()
+
+
+def get_callback_btns(*, btns: dict[str, str], sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+
+    for text, data in btns.items():
+        keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
+
+    return keyboard.adjust(*sizes).as_markup()
